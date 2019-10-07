1256cf35025b1b7a853acc9f13d7dc6f19d82636
Mukesh Poptani <mukesh@Mukeshs-MacBook-Pro.local>
2019-10-07 16:52:45 -0700

    updating mentor mentee assoc MENPS-17,18,19

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,47 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import MenteeData, MentorData, MentorMenteeAssoc
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 
-from .models import MenteeData, MentorData
-
+@csrf_exempt
 def index(request):
-    mentee_list = MenteeData.objects.order_by('mentee_id')
-    mentor_list = MentorData.objects.order_by('mentor_id')
-    output = []
-    output.append("Mentee, Mentor<br/>");
-    output.append(', '.join([str([mentee_list[i].firstname, mentor_list[i].firstname]) for i in range(len(mentor_list))]))
-    return HttpResponse(output)
+    # t = MenteeData.objects.get(mentee_id=1000)
+    # t.firstname = 'Mike'  # change field
+    # t.save() # this will update only
+
+    if request.method == "POST":
+    	print(request.POST['mentee_id'])
+    	mentorId = request.POST['mentor_id']
+    	menteeId = request.POST['mentee_id']
+
+    	mentorObj = MentorData.objects.get(mentor_id = mentorId)
+    	print(mentorObj.isactive)
+    	print(type(mentorObj.isavailable))
+    	mentorObj.isavailable = '\x00'
+    	mentorObj.save()
+
+    	menteeObj = MenteeData.objects.get(mentee_id = menteeId)
+    	menteeObj.isavailable = '\x00'
+    	menteeObj.save()
+
+    	assoc = MentorMenteeAssoc(mentor_id = mentorId, mentee_id = menteeId, match_date = datetime.now())
+    	print(assoc)
+    	assoc.save()
+    	return HttpResponse("SUCCESS")
+    else:
+	    mentee_list = MenteeData.objects.all().filter(isavailable = 1)
+	    mentor_list = MentorData.objects.all().filter(isavailable = 1)
+	    # output = []
+	    # output.append("Mentee, Mentor<br/>");
+	    # output.append(', '.join([str([mentee_list[i].firstname, mentor_list[i].firstname]) for i in range(len(mentor_list))]))
+
+	    template = loader.get_template('index.html')
+	    context = {
+	        'mentee_list': mentee_list,
+	        'mentor_list': mentor_list,
+	    }
+	    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
