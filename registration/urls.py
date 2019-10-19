f47287c6935e01ee1a51add72fce1cba4cd34d3f
Eshwar Nag <leshwar4@gmail.com>
2019-10-18 20:02:40 -0700

    Creating HTML Pages.
    MENPS-4
    MENPS-24
    
    Completed the Mentee Registration Page.
    Started developing Mentor Registration Page

--- registration/urls.py
+++ registration/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('mentee/', views.mentee_registration, name='mentee_registration'),
+    path('mentor/', views.mentor_registration, name='mentor_registration'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the registration index.")
+def mentee_registration(request):
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('mentee_registration.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
 
+def mentor_registration(request):
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('mentor_registration.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
