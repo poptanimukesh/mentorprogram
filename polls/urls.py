2998378316272dbb441591f40ece13ac992659b7
Raj Thaker <rthaker@C02MV12PFD58.lan>
2019-10-23 19:37:54 -0700

    Adding features for incomplete Previous records Jira ID: MENPS-85

--- polls/urls.py
+++ polls/urls.py
@@ -12,6 +12,15 @@ urlpatterns = [
 
     path('viewAssociations', views.viewAssociations, name='viewAssociations'),
 
+    path('viewMentors', views.viewMentors, name='viewMentors'),
+
+    path('mentorDetails/<int:id>', views.mentorDetails, name='mentorDetails'),
+
+    path('viewMentees', views.viewMentees, name='viewMentees'),
+
+    path('menteeDetails/<int:id>', views.menteeDetails, name='menteeDetails'),
+
     path('mentorActivity/', views.mentorActivity, name='mentorActivity'),
 
+    path('mentorHistory/', views.mentorHistory, name='mentorHistory'),
 ]
